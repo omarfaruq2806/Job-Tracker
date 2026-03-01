841baaf063e1940803630f010cc11a44f4010405
omarfaruk <mdomarfaruq2806@gmail.com>
2026-03-01 16:40:02 +0600

    btn-tab done

--- script/script.js
+++ script/script.js
@@ -1,1 +1,42 @@
-let 
+let currentTab = "all";
+
+const tabActive = ["bg-blue-600", "text-white"];
+const tabInactive = ["bg-white", "text-black", "border", "border-gray-300"];
+
+// sections
+const allSection = document.getElementById("allSection");
+const interviewSection = document.getElementById("interviewSection");
+const rejectedSection = document.getElementById("rejectedSection");
+
+function switchTab(tab) {
+  const tabs = ["all", "interview", "reject"];
+
+  for (let t of tabs) {
+    let tabName = document.getElementById("tab-" + t);
+    if (t === tab) {
+      tabName.classList.remove(...tabInactive);
+      tabName.classList.add(...tabActive);
+    } else {
+      tabName.classList.remove(...tabActive);
+      tabName.classList.add(...tabInactive);
+    }
+  }
+
+  //   const sections = {
+  //     all: allSection,
+  //     interview: interviewSection,
+  //     reject: rejectedSection,
+  //   };
+  const sections = [allSection, interviewSection, rejectedSection];
+  for (let section of sections) {
+    section.classList.add("hidden");
+  }
+  if (tab === "all") {
+    allSection.classList.remove("hidden");
+  } else if (tab === "interview") {
+    interviewSection.classList.remove("hidden");
+  } else if (tab === "reject") {
+    rejectedSection.classList.remove("hidden");
+  }
+}
+switchTab(currentTab);
